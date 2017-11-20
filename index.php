33fc3968230c038e9d26c87efaf13e63cf3d0282
campigoto <campigoto@hotmail.com>
2017-11-20 19:48:52 -0200

    mudei a frase

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
 
-<<<<<<< HEAD
-echo "Meu primeiro PHP. Alô Mundo.Beleza." ;
 
-echo "Informatica";
-=======
-echo "Meu primeiro PHP. Alô Mundo. Show" ;
-
-echo "Campigoto";
->>>>>>> 2097a7540773af90269fed8cd2127111aaf0aa01
+echo "Campigoto Informática";
 
  ?>
